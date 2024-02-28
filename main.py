c91c0871e36babc0be35c2c3e43863de713ba58a
eric <eric.boniardi@mail.polimi.it>
2024-02-28 13:55:21 -0600

    .

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 ########## main.py ##########
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Input(BaseModel):
+    text: str
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/submit")
+async def submit(input: Input):
+    return {"message": f"Data submitted is: {input.text}"}
